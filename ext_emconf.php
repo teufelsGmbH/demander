ff01caf2d1e73ba031a9285c86ce50dbeee94bd2
mabolek <mathias@lilio.com>
2020-12-08 20:17:42 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Demander',
+    'description' => 'Configurable, demand-based filtering framework with permalink-support for TYPO3.',
+    'category' => 'plugin',
+    'author' => 'Pixelant',
+    'author_email' => 'info@pixelant.net',
+    'author_company' => 'Pixelant',
+    'state' => 'alpha',
+    'createDirs' => '',
+    'clearCacheOnLoad' => true,
+    'version' => '1.0.0-alpha',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.6-10.4.99'
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ]
+];
