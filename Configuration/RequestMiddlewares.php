a6a73256b1802bf80ed59a01b0cef5ac4a0a4e4a
VladVilya <vladv161095@gmail.com>
2021-01-19 17:21:51 +0200

    [BUGFIX] added controlling structure

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -2,8 +2,14 @@
 
 return [
     'frontend' => [
-        'middleware-identifier' => [
+        'demander-request' => [
             'target' => \Pixelant\Demander\Middlewares\DemanderRequestMiddleware::class,
-        ],
-    ],
+            'after' => [
+                'typo3/cms-frontend/page-argument-validator'
+            ],
+            'before' => [
+                'yoast-seo-page-request'
+            ]
+        ]
+    ]
 ];
