90a20acaa55ab780a65ba95375dfe21cb8bc21fc
Mats Svensson <mats.svensson@resultify.se>
2021-02-23 13:36:57 +0100

    [TASK] Add Services.yaml file

--- Classes/DemandProvider/ContentDemandProvider.php
+++ Classes/DemandProvider/ContentDemandProvider.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Pixelant\Demander\DemandProvider;
 
+use TYPO3\CMS\Extbase\Persistence\Generic\Exception\NotImplementedException;
+
 class ContentDemandProvider implements DemandProviderInterface
 {
+    /**
+     * @return array
+     * @throws Exception\NotImplementedException
+     */
+    public function getDemand(): array
+    {
+        throw new NotImplementedException(__METHOD__, 1614082970);
+    }
 }
--- Classes/DemandProvider/PageDemandProvider.php
+++ Classes/DemandProvider/PageDemandProvider.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Pixelant\Demander\DemandProvider;
 
+use TYPO3\CMS\Extbase\Persistence\Generic\Exception\NotImplementedException;
+
 class PageDemandProvider implements DemandProviderInterface
 {
+    /**
+     * @return array
+     * @throws Exception\NotImplementedException
+     */
+    public function getDemand(): array
+    {
+        throw new NotImplementedException(__METHOD__, 1614083012);
+    }
 }
