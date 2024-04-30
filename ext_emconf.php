ffea1bcac58cabb28b9cd2cfa0e60e6343a46c50
Bastian <38100744+bh-teufels@users.noreply.github.com>
2024-04-30 11:02:55 +0200

    requirements for TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'alpha',
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '0.2.1',
+    'version' => '0.3.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.6-11.5.99'
+            'typo3' => '10.4.6-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => []
